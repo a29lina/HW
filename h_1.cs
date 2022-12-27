4e2e8b9c7c0df6c2eeef61f2feec367fc275d951
Alina <alishad29@mail.ru>
2022-12-27 23:12:40 +0300

    Result

---
+++ h_1.cs
@@ -0,0 +1,14 @@
+﻿
+
+void Result(string[] array_1, string[] array_2)
+{
+    int count = 0;
+    for (int i = 0; i < array_1.Length; i++)
+    {
+        if (array_1[i].Length <= 3)
+        {
+            array_2[count] = array_1[i];
+            count++;
+        }
+    }
+}
